b936d8e344b8861ca8d04ab524856f899cb1ed29
Colin Maher <colinmaher94@gmail.com>
2019-06-29 19:01:37 +0100

    Add basic login and registration UI.

--- app/src/main/java/com/example/fyp/MainActivity.kt
+++ app/src/main/java/com/example/fyp/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.fyp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_login_loginBtn.setOnClickListener {
+            val email = editText_login_email.text.toString()
+            val password = editText_login_password.text.toString()
+
+            Log.d("Login", "Email: $email")
+            Log.d("Login", "Password: $password")
+        }
+
+        textView_login_noAccount.setOnClickListener{
+            Log.d("Register", "Existing Account")
+        }
     }
 }
