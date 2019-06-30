d01ecec218070c9b8a70af9845d188cc3ad45a96
Colin Maher <colinmaher94@gmail.com>
2019-06-30 18:00:28 +0100

    Add basic login and registration UI
    Login and registration UI now functional. User interactions are now logged.

--- app/src/main/java/com/example/fyp/MainActivity.kt
+++ app/src/main/java/com/example/fyp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.fyp
 
+import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.TouchDelegate
+import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +20,14 @@ class MainActivity : AppCompatActivity() {
             val email = editText_login_email.text.toString()
             val password = editText_login_password.text.toString()
 
-            Log.d("Login", "Email: $email")
-            Log.d("Login", "Password: $password")
+            Log.d("Debug", "Attempting to log in with email: $email & password: $password")
         }
 
         textView_login_noAccount.setOnClickListener{
-            Log.d("Register", "Existing Account")
+            Log.d("Debug", "Create account button clicked")
+
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
         }
     }
 }
